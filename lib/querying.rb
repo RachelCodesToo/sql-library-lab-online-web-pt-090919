42b286686f973b89bce872797285174797ab70e5
Anne Fausto <a.r.fausto@gmail.com>
2020-01-14 16:16:53 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,7 +21,7 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM authors, subgenres, series
-  WHERE authors.id = series.authors_id
+  WHERE authors.id = series.author_id
   AND subgenres.id = series.subgenre_id"
 end
 
